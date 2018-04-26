97c918457ec0b660ed7706aca7d6bf18c15b23a7
adrien ZEMMA <adrien.zemma@epitech.eu>
2018-04-26 14:59:51 +0200

    adding socket support

---
+++ src/transport.cpp
@@ -0,0 +1,89 @@
+/*
+** EPITECH PROJECT, 2018
+** plazza
+** File description:
+** transport
+*/
+
+#include "../inc/transport.hpp"
+#include <sys/un.h>
+
+Transport &operator<<(Transport &flux, const std::string &text)
+{
+	std::cout << "try send"<<std::endl;
+	flux.send(text);
+	return flux;
+}
+
+Transport &operator>>(Transport &flux, std::string &text)
+{
+	std::cout << "try get"<<std::endl;
+	text = flux.reading();
+	return flux;
+}
+
+Transport::Transport(size_t status)
+{
+	if (status == 1)
+		createServer();
+	else
+		connectClient();
+}
+
+Transport::~Transport()
+{}
+
+void	Transport::send(std::string txt)
+{
+	int rc;
+	if (*txt.end() != '\n')
+		txt += '\n';
+	rc = write(fd, txt.data(), txt.size());
+	if (rc == -1) {
+		perror("write error");
+	}
+}
+
+std::string	Transport::reading()
+{
+	int rc;
+	char	buf[2];
+	std::string	tmp;
+	while (read(fd, buf, 1))
+	{
+		tmp += buf[0];
+		if (buf[0] == '\n')
+			return tmp;
+	}
+	return tmp;
+}
+
+void	Transport::createServer()
+{
+	struct sockaddr_un addr;
+	char buf[100];
+	int cl;
+	int rc;
+	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, "test.sock", sizeof(addr.sun_path)-1);
+	bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+	listen(fd, 5);
+	fd = accept(fd, NULL, NULL);
+	
+}
+
+void	Transport::connectClient()
+{
+	struct sockaddr_un addr;
+	char buf[100];
+	int cl;
+	int rc;
+	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, "test.sock", sizeof(addr.sun_path)-1);
+	connect(fd, (struct sockaddr*)&addr, sizeof(addr));
+}
+
