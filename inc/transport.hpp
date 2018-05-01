dfb31b9d15e1077cb6bc926135580b399ce014ac
Adrien Zemma <adrien.zemma@epitech.eu>
2018-05-01 16:07:29 +0200

    transport object build

--- inc/transport.hpp
+++ inc/transport.hpp
@@ -8,26 +8,27 @@
 #ifndef TRANSPORT_HPP_
 	#define TRANSPORT_HPP_
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/un.h>
 #include <iostream>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
+#include <stdio.h>
+#include <netdb.h>
 #include <memory>
 #include <map>
-#include <sys/types.h>
-
 
 class Transport
 {
 public:
 	Transport();
-	~Transport();
 	Transport(std::string socketFile);
+	Transport(std::string socketFile, int nbClient);
+	~Transport();
 	size_t	getFdTarget();
 	void	send(std::string);
 	std::string	reading();
